b12860b430c54c7bdd0e1e84ed376eb17f01428e
Joshua <joshuanweze270@gmail.com>
2022-11-01 07:54:12 +0100

    Added javascript file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Landing
+
+## About
+
+Landing is a free website theme. Landing is released under the MIT license, which means you can use them for any purpose, even for commercial projects.
+
+## Download and Installation
+
+To begin using this theme, follow any of these steps:
+* Clone the repo: `git clone https://github.com/Joshua-Nweze/Landing.git`
+* [Fork, Clone, or Download on GitHub](https://github.com/Joshua-Nweze/Landing)
+
+## Usage
+
+After downloading, simply edit the HTML and CSS files included with the theme in your favorite text editor to make changes. To preview the changes you make to the code, you can open the `index.html` file in your web browser.
+
+## Bugs and Issues
+
+Have a bug or an issue with this theme? [Open a new issue](https://github.com/Joshua-Nweze/Landing) here on GitHub.
+
+## Copyright and License
+
+Copyright 2022 [Joshua Nweze](https://github.com/Joshua-Nweze). Code released under the [MIT](https://github.com/Joshua-Nweze/Landing/blob/main/LICENSE) license.
---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Collapse navbar when nav item is clicked
+let navLink = document.querySelectorAll('.nav-link');
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', () => {
+        document.getElementById('navbarSupportedContent').classList.remove('show')
+    });
+}
+
+//Contact form validation
+let fullName = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+let submit = document.getElementById('submit');
+
+let regEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+
+submit.addEventListener('click', () => {
+    if (!fullName.value) {
+        fullName.classList.add('error');
+
+        return false;
+    } else {
+        fullName.classList.remove('error');
+    }
+
+    if (!regEx.test(email.value)) {
+        email.classList.add('error');
+
+        return false;
+    }else {
+        email.classList.remove('error');
+    }
+
+    if (!message.value) {
+        message.classList.add('error');
+
+        return false;
+    }else {
+        message.classList.remove('error');
+    }
+
+    return true
+
+})
+
